2f17d9432593309628e3026be36fc27e5cd2717b
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-12-19 18:04:20 +0100

    ejercicio service1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'servicio1';
+export class AppComponent implements OnInit {
+  productos: any;
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.productos = this.productService.retornar();
+
+  }
 }
